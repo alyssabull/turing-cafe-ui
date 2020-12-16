67eac8e843c1345340586e9ec45ce1f8f7ba7136
Alyssa Bull <alyssabull@gmail.com>
2020-12-16 10:54:37 -0700

    Refactor app integration test to reflect post request

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,38 +1,57 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom';
 import App from './App.js';
+import { fetchAllReservations, postNewReservation } from '../apiCalls.js';
+jest.mock('../apiCalls.js')
 
 describe('App', () => {
-  const mockAddNewReservation = jest.fn()
 
   beforeEach(() => {
-    render(<App addNewReservation={mockAddNewReservation}/>)
+    const mockResy = [{
+      id: 1,
+      name: 'John',
+      date: '12/30',
+      time: '6:45',
+      number: '4'
+    }]
+
+    fetchAllReservations.mockResolvedValueOnce(mockResy);
+
+    render(<App />)
   })
 
-  it('should be able to post a new Reservation', () => {
+  it('should be able to post a new Reservation', async () => {
+    const mockNewResy = {
+      id: 2,
+      name: 'Hank',
+      date: '1/17',
+      time: '4:30',
+      number: '6'
+    }
+
+    postNewReservation.mockResolvedValueOnce(mockNewResy)
+
     const nameInput = screen.getByPlaceholderText('Name')
     const dateInput = screen.getByPlaceholderText('Date (mm/dd)')
     const timeInput = screen.getByPlaceholderText('Time --:--')
     const numberInput = screen.getByPlaceholderText('Number of Guests')
     const submitButton = screen.getByText('SUBMIT RESERVATION')
 
-    const userNameInput = 'Alyssa'
-    const userDateInput = '12/25'
-    const userTimeInput = '6:00'
-    const userNumberInput = '2'
 
-    fireEvent.change(nameInput, {target: { value: userNameInput }})
-    fireEvent.change(dateInput, {target: { value: userDateInput }})
-    fireEvent.change(timeInput, {target: { value: userTimeInput }})
-    fireEvent.change(numberInput, {target: { value: userNumberInput }})
+    fireEvent.change(nameInput, {target: { value: mockNewResy.name }})
+    fireEvent.change(dateInput, {target: { value: mockNewResy.date }})
+    fireEvent.change(timeInput, {target: { value: mockNewResy.time }})
+    fireEvent.change(numberInput, {target: { value: mockNewResy.number }})
     fireEvent.click(submitButton)
 
-    expect(nameInput).toBeInTheDocument()
-    expect(dateInput).toBeInTheDocument()
-    expect(timeInput).toBeInTheDocument()
-    expect(numberInput).toBeInTheDocument()
-    expect(screen.getByText('CANCEL')).toBeInTheDocument()
+    const newResyName = await waitFor(() => screen.getByText('Hank'))
+    const newResyDate = await waitFor(() => screen.getByText('1/17'))
+    const newResyTime = await waitFor(() => screen.getByText('4:30'))
+
+    expect(newResyName).toBeInTheDocument()
+    expect(newResyDate).toBeInTheDocument()
+    expect(newResyTime).toBeInTheDocument()
   })
 })
