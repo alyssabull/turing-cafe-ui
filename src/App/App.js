c8efb396256d44f4e078cc5a090de0e82a4b2ef8
Alyssa Bull <alyssabull@gmail.com>
2020-12-16 09:28:41 -0700

    Set up app and fetch reservations

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import Reservations from '../Reservations/Reservations.js';
+import { fetchAllReservations } from '../apiCalls.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetchAllReservations().then(allReservations => console.log(allReservations)).catch(err => err.message)
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +23,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+        
         </div>
       </div>
     )
